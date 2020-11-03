57652cc2b06bfdb43614f69d37b91b28340efbdd
Asgar Zagitov <ihateyouthor@gmail.com>
2020-11-03 05:55:38 +0300

    added pid write

--- src/main/java/fi/lipp/greatheart/gateway/GatewayApplication.java
+++ src/main/java/fi/lipp/greatheart/gateway/GatewayApplication.java
@@ -2,12 +2,15 @@ package fi.lipp.greatheart.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication springApplication = new SpringApplication(GatewayApplication.class);
+		springApplication.addListeners(new ApplicationPidFileWriter());
+		springApplication.run(args);
 	}
 
 }
